48bea306085c0bc289f5d3bff8c8d863e01215c8
jwanahmadlp <jwankaderisasi25@gmail.com>
2026-07-08 15:54:00 +0700

    Menambahkan fungsi CRUD Update dan Delete SQLite
    Menambahkan fungsi update_mood() dan delete_mood() pada database SQLite sebagai bagian dari implementasi CRUD Tugas 11.

--- Tugas-09/Renjana/Manager/database.py
+++ Tugas-09/Renjana/Manager/database.py
@@ -25,7 +25,31 @@ def save_mood(result):
 def get_history():
     conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
-    c.execute("SELECT date, result FROM moods ORDER BY id DESC")
+    c.execute("SELECT id, date, result FROM moods ORDER BY id DESC")
     rows = c.fetchall()
     conn.close()
     return rows
+
+def update_mood(id, new_result):
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+
+    today = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    c.execute("""
+        UPDATE moods
+        SET date = ?, result = ?
+        WHERE id = ?
+    """, (today, new_result, id))
+
+    conn.commit()
+    conn.close()
+
+def delete_mood(id):
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+
+    c.execute("DELETE FROM moods WHERE id = ?", (id,))
+
+    conn.commit()
+    conn.close()
